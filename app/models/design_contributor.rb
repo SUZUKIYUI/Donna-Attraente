9e265ef0a31215bad76345f2bbe8fc1c46bec330
SUZUKIYUI <iaguminano@docomo.ne.jp>
2021-06-27 16:25:42 +0000

    [Add]デザイン投稿者バリデーション設定

--- app/models/design_contributor.rb
+++ app/models/design_contributor.rb
@@ -4,6 +4,10 @@ class DesignContributor < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
+  validates :pen_name, presence: true
+  validates :profile_image, presence: false
+  validates :self_introduction, presence: false
+
   has_many :posts, dependent: :destroy
 
   attachment :profile_image
