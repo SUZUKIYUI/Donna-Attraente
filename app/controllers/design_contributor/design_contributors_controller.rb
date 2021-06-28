4c5c739621534a4d225bebd2ac0174a4f654eb63
SUZUKIYUI <iaguminano@docomo.ne.jp>
2021-06-28 14:01:56 +0000

    [Add]デザイン投稿者/updateアクション記述

--- app/controllers/design_contributor/design_contributors_controller.rb
+++ app/controllers/design_contributor/design_contributors_controller.rb
@@ -9,6 +9,19 @@ class DesignContributor::DesignContributorsController < ApplicationController
   end
 
   def update
+    @design_contributor = DesignContributor.find(params[:id])
+    if @design_contributor.update(design_contributor_params)
+      redirect_to design_contributor_design_contributor_path(@design_contributor.id)
+    else
+      render :edit
+    end
+  end
+
+  # この下に何も記述しないこと！
+  private
+
+  def design_contributor_params
+    params.require(:design_contributor).permit(:profile_image, :pen_name, :email, :self_introduction)
   end
 
 end
