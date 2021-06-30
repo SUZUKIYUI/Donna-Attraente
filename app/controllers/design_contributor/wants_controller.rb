615c3647c3148debd1afeeb3245106c86e052d93
SUZUKIYUI <iaguminano@docomo.ne.jp>
2021-06-30 13:32:48 +0000

    [Add]create・destroyアクション定義

--- app/controllers/design_contributor/wants_controller.rb
+++ app/controllers/design_contributor/wants_controller.rb
@@ -1,9 +1,15 @@
 class DesignContributor::WantsController < ApplicationController
 
   def create
+    @post = Post.find(params[:post_id])
+    Want.create(design_contributor_id: current_design_contributor.id, post_id: @post.id)
+    redirect_to design_contributor_post_path(@post.id)
   end
 
   def destroy
+    @post = Post.find(params[:post_id])
+    Want.find_by(design_contributor_id: current_design_contributor.id, post_id: @post.id).destroy
+    redirect_to design_contributor_post_path(@post.id)
   end
 
 end
