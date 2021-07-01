e1b1baa76a69a46a0ed120037d85cd246b6243f4
SUZUKIYUI <iaguminano@docomo.ne.jp>
2021-07-01 12:24:43 +0000

    [Add]companyモデルにバリデーション設定

--- app/models/company.rb
+++ app/models/company.rb
@@ -3,6 +3,14 @@ class Company < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-
+  # ----------------------------------------------
+  validates :company_name, presence: true
+  validates :company_name_kana, presence: true
+  validates :postal_code, presence: true
+  validates :address, presence: true
+  validates :phone_number, presence: true
+  validates :profile_image, presence: false
+  validates :company_introduction, presence: false
+  # ----------------------------------------------
   attachment :profile_image
 end
