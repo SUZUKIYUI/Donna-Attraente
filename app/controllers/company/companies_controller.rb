c84a3b0527650f79d0d71a39f3a6397a8385c65b
SUZUKIYUI <iaguminano@docomo.ne.jp>
2021-07-04 04:17:30 +0000

    [Add]企業側/updateアクション定義・ストロングパラメータ設定

--- app/controllers/company/companies_controller.rb
+++ app/controllers/company/companies_controller.rb
@@ -13,6 +13,19 @@ class Company::CompaniesController < ApplicationController
   end
 
   def update
+    @company = Company.find(params[:id])
+    if @company.update(company_params)
+      redirect_to company_company_path(@company.id)
+    else
+      render :edit
+    end
+  end
+
+  # この下に何も記述しないこと！
+  private
+
+  def company_params
+    params.require(:company).permit(:profile_image, :company_name, :company_name_kana, :postal_code, :address, :phone_number, :email, :company_introduction)
   end
 
 end
