ab873c91395594df55d42356318fd45500f97d1d
SUZUKIYUI <iaguminano@docomo.ne.jp>
2021-07-04 06:42:37 +0000

    [Add]企業側/バリデーション設定

--- app/models/offer.rb
+++ app/models/offer.rb
@@ -1,4 +1,7 @@
 class Offer < ApplicationRecord
+  validates :offer_comment, presence: true
+  validates :offer_status, presence: true
+  # --------------------------------------
   enum offer_status: {
     negotiation: 0,
     established: 1,
