f15aee382207ebdc8930b57d79173827cf70bea6
SUZUKIYUI <iaguminano@docomo.ne.jp>
2021-07-04 06:37:09 +0000

    [Add]企業側/showアクション・作品詳細画面に条件分岐設定（オファー関連）

--- app/controllers/company/posts_controller.rb
+++ app/controllers/company/posts_controller.rb
@@ -6,6 +6,19 @@ class Company::PostsController < ApplicationController
 
   def show
     @post = Post.find(params[:id])
+    # オファーモデルが存在するか
+    if Offer.exists?
+      # 交渉中のオファーが存在すれば@offerへ定義
+      if Offer.where(post_id: @post.id, offer_status: 0).exists?
+        @offer = Offer.find_by(post_id: @post.id, offer_status: 0)
+      # 交渉成立オファーが存在すれば@offerへ定義
+      elsif Offer.where(post_id: @post.id, offer_status: 1).exists?
+        @offer = Offer.find_by(post_id: @post.id, offer_status: 1)
+      # 交渉不成立オファーが存在すれば@offersへ定義
+      elsif Offer.where(post_id: @post.id, offer_status: 2).exists?
+        @offers = Offer.where(post_id: @post.id, offer_status: 2)
+      end
+    end
   end
 
 end
