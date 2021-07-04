300a3f146fdbd2dc6a6cf28d11bc9a67e674252f
SUZUKIYUI <iaguminano@docomo.ne.jp>
2021-07-04 09:51:29 +0000

    [Add]企業側/createアクション定義・ストロングパラメータ設定

--- app/controllers/company/offers_controller.rb
+++ app/controllers/company/offers_controller.rb
@@ -6,6 +6,22 @@ class Company::OffersController < ApplicationController
   end
 
   def create
+    @post = Post.find(params[:post_id])
+    @offer = Offer.new(offer_params)
+    @offer.company_id = current_company.id
+    @offer.post_id = @post.id
+    if @offer.save
+      redirect_to company_post_path(@post.id)
+    else
+      render :new
+    end
+  end
+
+  # この下に何も記述しないこと！
+  private
+
+  def offer_params
+    params.require(:offer).permit(:offer_comment, :offer_status)
   end
 
 end
