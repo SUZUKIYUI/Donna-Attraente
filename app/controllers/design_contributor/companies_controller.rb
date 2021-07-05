7962e411b957ae60e5413460d8cb000814667245
SUZUKIYUI <iaguminano@docomo.ne.jp>
2021-07-05 13:38:21 +0000

    [Add]editアクション・オファー編集画面作成

--- app/controllers/design_contributor/companies_controller.rb
+++ app/controllers/design_contributor/companies_controller.rb
@@ -6,6 +6,12 @@ class DesignContributor::CompaniesController < ApplicationController
 
   def show
     @company = Company.find(params[:id])
+    # ----------------------------------
+    if Offer.exists?
+      if Offer.where(company_id: @company.id, offer_status: 1).exists?
+        @offers = Offer.where(post_id: @post.id, offer_status: 1)
+      end
+    end
   end
 
 end
--- app/controllers/design_contributor/offers_controller.rb
+++ app/controllers/design_contributor/offers_controller.rb
@@ -1,6 +1,8 @@
 class DesignContributor::OffersController < ApplicationController
 
   def edit
+    @post = Post.find(params[:post_id])
+    @offer = Offer.find(params[:id])
   end
 
   def update
