8c91a2169967c0c47c8d59814cbd58a44c1f3e09
SUZUKIYUI <iaguminano@docomo.ne.jp>
2021-07-06 11:05:30 +0000

    [Fix]offer_statusの値変更

--- app/controllers/design_contributor/posts_controller.rb
+++ app/controllers/design_contributor/posts_controller.rb
@@ -25,14 +25,14 @@ class DesignContributor::PostsController < ApplicationController
     # オファーモデルが存在するか
     if Offer.exists?
       # 交渉中のオファーが存在すれば@offerへ定義
-      if Offer.where(post_id: @post.id, offer_status: 0).exists?
-        @offer = Offer.find_by(post_id: @post.id, offer_status: 0)
+      if Offer.where(post_id: @post.id, offer_status: "negotiation").exists?
+        @offer = Offer.find_by(post_id: @post.id, offer_status: "negotiation")
       # 交渉成立オファーが存在すれば@offerへ定義
-      elsif Offer.where(post_id: @post.id, offer_status: 1).exists?
-        @offer = Offer.find_by(post_id: @post.id, offer_status: 1)
+      elsif Offer.where(post_id: @post.id, offer_status: "established").exists?
+        @offer = Offer.find_by(post_id: @post.id, offer_status: "established")
       # 交渉不成立オファーが存在すれば@offersへ定義
-      elsif Offer.where(post_id: @post.id, offer_status: 2).exists?
-        @offers = Offer.where(post_id: @post.id, offer_status: 2)
+      elsif Offer.where(post_id: @post.id, offer_status: "failure").exists?
+        @offers = Offer.where(post_id: @post.id, offer_status: "failure")
       end
     end
   end
