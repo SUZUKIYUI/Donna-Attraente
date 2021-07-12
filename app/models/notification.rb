d4ba234d2415eab4fbef49e7461355e97e4cdc8f
SUZUKIYUI <iaguminano@docomo.ne.jp>
2021-07-12 12:26:33 +0000

    [Add]notification/通知機能のアソシエーション設定

--- app/models/notification.rb
+++ app/models/notification.rb
@@ -1,2 +1,10 @@
 class Notification < ApplicationRecord
+    default_scope -> {order(created_at: :desc)}
+    belongs_to :post, optional: true
+    belongs_to :comment, optional: true
+    belongs_to :offer, optional: true
+    belongs_to :visitor_design_contributor, class_name: "DesignContributor", foreign_key: "visitor_design_contributor_id", optional: true
+    belongs_to :visited_design_contributor, class_name: "DesignContributor", foreign_key: "visited_design_contributor_id", optional: true
+    belongs_to :visitor_company, class_name: "Company", foreign_key: "visitor_company_id", optional: true
+    belongs_to :visited_company, class_name: "Company", foreign_key: "visited_company_id", optional: true
 end
