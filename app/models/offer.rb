52af4dcfc58c5e4ad4f7a0cc1258ce9e66adaf33
SUZUKIYUI <iaguminano@docomo.ne.jp>
2021-07-12 12:34:18 +0000

    [Add]offer/通知機能のアソシエーション設定

--- app/models/offer.rb
+++ app/models/offer.rb
@@ -2,6 +2,7 @@ class Offer < ApplicationRecord
   # ---------------------------
   belongs_to :company
   belongs_to :post
+  has_many :notifications, dependent: :destroy
   # --------------------------------------
   validates :offer_comment, presence: true
   validates :offer_status, presence: true
