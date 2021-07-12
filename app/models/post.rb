f8b7eb3580bef422c2e1fa3822cafdb088223390
SUZUKIYUI <iaguminano@docomo.ne.jp>
2021-07-12 13:57:02 +0000

    [Add]post/いいね通知作成メソッド定義

--- app/models/post.rb
+++ app/models/post.rb
@@ -16,17 +16,31 @@ class Post < ApplicationRecord
   def offered_by?
     Offer.exists?
   end
-  # --------------交渉中のオファーが存在するか--------------
+  # -------------------交渉中のオファーが存在するか-------------------
   def offered_negotiation_by?(post_id)
     offers.where(post_id: post_id, offer_status: :negotiation).exists?
   end
-  # -------------交渉成立オファーが存在するか--------------
+  # -------------------交渉成立オファーが存在するか-------------------
   def offered_established_by?(post_id)
     offers.where(post_id: post_id, offer_status: :established).exists?
   end
-  # ------------交渉不成立オファーが存在するか-------------
+  # ------------------交渉不成立オファーが存在するか------------------
   def offered_failure_by?(post_id)
     offers.where(post_id: post_id, offer_status: :failure).exists?
   end
+
+  # -----------------------いいねの通知を作成-------------------------
+  def create_notification_like!(current_design_contributor)
+    notification = current_design_contributor.active_notifications.new(
+      post_id: id,
+      visited_design_contributor_id: design_contributor_id,
+      action: "like"
+    )
+    if notification.visitor_design_controbutor_id == notification.visited_design_contributor_id
+      notification.checked = true
+    end
+    notification.save if notification.valid?
+  end
+
 
 end
