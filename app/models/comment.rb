d6d577b3dbdb4341421f77278554370bb758514d
SUZUKIYUI <iaguminano@docomo.ne.jp>
2021-07-12 11:46:12 +0000

    [Add]comment/通知機能のアソシエーション設定

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -2,6 +2,7 @@ class Comment < ApplicationRecord
   # ----------------------------
   belongs_to :design_contributor
   belongs_to :post
+  has_many :notifications, dependent: :destroy
   # --------------------------------
   validates :comment, presence: true
 
