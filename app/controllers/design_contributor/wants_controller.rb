df9069762f9b1994470ab92c3ce785d4ea284c86
SUZUKIYUI <iaguminano@docomo.ne.jp>
2021-07-19 14:37:23 +0000

    [Add]wantsコントローラ/createアクションにいいねの通知作成メソッド記述

--- app/controllers/design_contributor/wants_controller.rb
+++ app/controllers/design_contributor/wants_controller.rb
@@ -3,6 +3,8 @@ class DesignContributor::WantsController < ApplicationController
   def create
     @post = Post.find(params[:post_id])
     Want.create(design_contributor_id: current_design_contributor.id, post_id: @post.id)
+    # いいね （なにこれ欲しい）通知作成
+    @post.create_notification_want!(current_design_contributor)
     redirect_to design_contributor_post_path(@post.id)
   end
 
