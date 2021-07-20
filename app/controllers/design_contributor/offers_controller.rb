c07b252f72eab40cf2fc4b9842c580182c77b3bc
SUZUKIYUI <iaguminano@docomo.ne.jp>
2021-07-20 14:59:24 +0000

    [Update]offersコントローラ/updateアクションのオファー（返信）の通知作成メソッド

--- app/controllers/design_contributor/offers_controller.rb
+++ app/controllers/design_contributor/offers_controller.rb
@@ -9,7 +9,7 @@ class DesignContributor::OffersController < ApplicationController
     @post = Post.find(params[:post_id])
     @offer = Offer.find(params[:id])
     if @offer.update(offer_params)
-      @post.create_notification_offer!(current_design_contributor, @offer.id)
+      @post.create_notification_offer!(current_design_contributor, @offer.id, @offer.company.id)
       redirect_to design_contributor_post_path(@post.id)
     else
       render :edit
