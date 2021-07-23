b8960ca26572fd8a0d65c91c22a28b812b5dcb2b
SUZUKIYUI <iaguminano@docomo.ne.jp>
2021-07-23 14:01:40 +0000

    [Update]デザイン投稿者側/indexアクションの@notifications変数を更新

--- app/controllers/design_contributor/notifications_controller.rb
+++ app/controllers/design_contributor/notifications_controller.rb
@@ -3,6 +3,8 @@ class DesignContributor::NotificationsController < ApplicationController
   # デザイン投稿者からデザイン投稿者にきたいいね・コメントの通知一覧
   def index
     @notifications = current_design_contributor.passive_notifications
+    # 以下@notifications更新
+    @notifications = @notifications.where.not(design_contributor_id: current_design_contributor.id)
     @notifications.where(checked: false).each do |notification|
       notification.update_attributes(checked: true)
     end
