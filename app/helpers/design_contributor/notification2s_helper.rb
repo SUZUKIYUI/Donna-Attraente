37d6269d42b3d1f0f60dbb66a1322f80ad060137
SUZUKIYUI <iaguminano@docomo.ne.jp>
2021-07-25 11:55:57 +0000

    [Fix]デザイン投稿者側/notifications_helperのnotification_form(notification)「tag」修正

--- app/helpers/design_contributor/notification2s_helper.rb
+++ app/helpers/design_contributor/notification2s_helper.rb
@@ -1,2 +1,7 @@
 module DesignContributor::Notification2sHelper
+  
+  def notification_form(notification)
+    
+  end
+  
 end
--- app/helpers/design_contributor/notifications_helper.rb
+++ app/helpers/design_contributor/notifications_helper.rb
@@ -12,11 +12,11 @@ module DesignContributor::NotificationsHelper
                 tag.a(@visitor.pen_name, href: design_contributor_design_contributor_path(@visitor)) + "が" + tag.a("あなたのデザイン", href: design_contributor_post_path(notification.post_id)) + "になにこれ欲しいを押しました"
             when "comment" then
                 @comment = Comment.find_by(id: @visitor_comment)&.comment
-                teg.a(@visitor.pen_name, href: design_contributor_design_contributor_path(@visitor)) + "が" + tag.a("あなたのデザイン", href: design_contributor_post_path(notification.post_id)) + "にコメントしました"
+                tag.a(@visitor.pen_name, href: design_contributor_design_contributor_path(@visitor)) + "が" + tag.a("あなたのデザイン", href: design_contributor_post_path(notification.post_id)) + "にコメントしました"
         end
     end
 
-    def unchecked_notifications
-        @notifications = current_design_contributor.passive_notifications.where(checked: false)
-    end
+    # def unchecked_notifications
+    #     @notifications = current_design_contributor.passive_notifications.where(checked: false)
+    # end
 end
