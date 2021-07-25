1f392e9ea5360ff9d83dc210d5991ae3689edd64
SUZUKIYUI <iaguminano@docomo.ne.jp>
2021-07-25 10:09:29 +0000

    [Remove]notification2sコントローラ/destroyアクション削除

--- app/controllers/design_contributor/notification2s_controller.rb
+++ app/controllers/design_contributor/notification2s_controller.rb
@@ -8,10 +8,4 @@ class DesignContributor::Notification2sController < ApplicationController
     end
   end
 
-  def destroy
-    # 企業からデザイン投稿者にきたオファーの通知全部削除
-    @notification2s = current_design_contributor.notification2s.destroy_all
-    redirect_to design_contributor_notification2s_path
-  end
-
 end
