920138944fd2d2247b1d2d9cd016979e3e9e4b4b
SUZUKIYUI <iaguminano@docomo.ne.jp>
2021-07-25 10:01:30 +0000

    [Remove]notificationsコントローラ/destroyアクション削除

--- app/controllers/company/notifications_controller.rb
+++ app/controllers/company/notifications_controller.rb
@@ -8,10 +8,4 @@ class Company::NotificationsController < ApplicationController
     end
   end
 
-  # デザイン投稿者から企業にきたオファーの返信通知全部削除
-  def destroy
-    @notifications = current_company.passive_notifications.destroy_all
-    redirect_to company_notifications_path
-  end
-
 end
