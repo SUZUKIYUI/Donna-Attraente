63d0cab407b2afc89526f64447c0a517097d1156
SUZUKIYUI <iaguminano@docomo.ne.jp>
2021-07-25 11:41:59 +0000

    [Fix]デザイン投稿者側/notifications_helperのnotification_form(notification)書き直し

--- app/helpers/design_contributor/notifications_helper.rb
+++ app/helpers/design_contributor/notifications_helper.rb
@@ -1,19 +1,21 @@
 module DesignContributor::NotificationsHelper
+
     def notification_form(notification)
         # 通知を送った人
-        @design_contributor = notification.design_contributor
+        @visitor = notification.visitor
         @comment = nil
-        @design_contributor_comment = notification.comment_id
+        # 通知で送られてきたコメントのid
+        @visitor_comment = notification.comment_id
 
         case notification.action
             when "want" then
-                tag.a(@design_contributor.pen_name, href: design_contributor_design_contributor_path(@design_contributor)) + "が" + tag.a("あなたのデザイン", href: design_contributor_post_path(notification.post_id)) + "になにこれ欲しいを押しました"
+                tag.a(@visitor.pen_name, href: design_contributor_design_contributor_path(@visitor)) + "が" + tag.a("あなたのデザイン", href: design_contributor_post_path(notification.post_id)) + "になにこれ欲しいを押しました"
             when "comment" then
-                @comment = Comment.find_by(id: @design_contributor_comment)&.comment
-                teg.a(@design_contributor.pen_name, href: design_contributor_design_contributor_path(@design_contributor)) + "が" + tag.a("あなたのデザイン", href: design_contributor_post_path(notification.post_id)) + "にコメントしました"
+                @comment = Comment.find_by(id: @visitor_comment)&.comment
+                teg.a(@visitor.pen_name, href: design_contributor_design_contributor_path(@visitor)) + "が" + tag.a("あなたのデザイン", href: design_contributor_post_path(notification.post_id)) + "にコメントしました"
         end
     end
-    
+
     def unchecked_notifications
         @notifications = current_design_contributor.passive_notifications.where(checked: false)
     end
