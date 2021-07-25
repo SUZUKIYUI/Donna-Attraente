5b27b1a6e54684c18c146d9bba4ca747f0dc329a
SUZUKIYUI <iaguminano@docomo.ne.jp>
2021-07-25 08:58:58 +0000

    [Update]notificationsコントローラ/@notificationsの更新

--- app/controllers/design_contributor/notifications_controller.rb
+++ app/controllers/design_contributor/notifications_controller.rb
@@ -2,9 +2,9 @@ class DesignContributor::NotificationsController < ApplicationController
 
   # デザイン投稿者からデザイン投稿者にきたいいね・コメントの通知一覧
   def index
-    @notifications = current_design_contributor.passive_notifications
-    # 以下@notifications更新
-    @notifications = @notifications.where.not(design_contributor_id: current_design_contributor.id)
+    notifications = current_design_contributor.passive_notifications
+    # @notifications = 自分宛の全ての通知（自分から自分への通知は含まない）
+    @notifications = notifications.where.not(visitor_id: current_design_contributor.id)
     @notifications.where(checked: false).each do |notification|
       notification.update_attributes(checked: true)
     end
