3976e0eb19e4122523720afc38af1138ceef8ff7
SUZUKIYUI <iaguminano@docomo.ne.jp>
2021-07-25 12:27:53 +0000

    [Add]デザイン投稿者側/notification2s_helperにnotification2_form(notification2)メソッド設定

--- app/helpers/design_contributor/notification2s_helper.rb
+++ app/helpers/design_contributor/notification2s_helper.rb
@@ -1,7 +1,10 @@
 module DesignContributor::Notification2sHelper
-  
-  def notification_form(notification)
-    
+
+  # 企業からきたオファー のみの通知を表示
+  def notification2_form(notification2)
+    @visitor = notification2.company
+
+    tag.a(@visitor.company_name, href: design_contributor_company_path(@visitor)) + "様から" + tag.a("あなたのデザイン", href: design_contributor_post_path(notification2.post_id)) + "にオファーが届きました"
   end
-  
+
 end
