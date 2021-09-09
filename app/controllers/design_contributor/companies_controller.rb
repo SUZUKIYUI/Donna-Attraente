582075ecbf01435eb5bbddc61539a4c1c36ceba2
SUZUKIYUI <iaguminano@docomo.ne.jp>
2021-09-09 12:28:28 +0000

    [Add]企業詳細画面レイアウト調整・[Fix]companyコントローラのshowアクション・link_toのpath記述修正

--- app/controllers/design_contributor/companies_controller.rb
+++ app/controllers/design_contributor/companies_controller.rb
@@ -10,7 +10,7 @@ class DesignContributor::CompaniesController < ApplicationController
     if Offer.exists?
       # 交渉成立オファーが存在すれば@offersへ定義
       if Offer.where(company_id: @company.id, offer_status: :established).exists?
-        @offers = Offer.where(post_id: @post.id, offer_status: :established)
+        @offers = Offer.where(company_id: @company.id, offer_status: :established)
       end
     end
   end
