6528aefd80ed52b7512919a7feda2ed5e412a442
SUZUKIYUI <iaguminano@docomo.ne.jp>
2021-09-15 14:01:00 +0000

    [Add]Top画面レイアウト調整

--- app/controllers/homes_controller.rb
+++ app/controllers/homes_controller.rb
@@ -2,6 +2,7 @@ class HomesController < ApplicationController
 
   def top
     @new_posts = Post.all.order(created_at: :desc).limit(12)
+    @want_posts = Post.find(Want.group(:post_id).order('count(post_id) desc').limit(3).pluck(:post_id))
   end
 
   def about
