f9e57cc438992ae3cbcc34ac1f2dd78393d871de
SUZUKIYUI <iaguminano@docomo.ne.jp>
2021-10-14 14:20:35 +0000

    [Fix]通知画面記述調整

--- app/helpers/company/notifications_helper.rb
+++ app/helpers/company/notifications_helper.rb
@@ -2,8 +2,9 @@ module Company::NotificationsHelper
 
   # デザイン投稿者からきたオファーのみの通知を表示
   def notification_form(notification)
-    if notification.action == "offer"
-      tag.a("#{notification.design_contributor.pen_name}様のデザイン", href: company_post_path(notification.post_id)) + "の" + tag.a("オファー結果", href: company_company_path(current_company)) + "が届いています"
+    case notification.action
+      when "offer" then
+        tag.a("#{notification.design_contributor.pen_name}様のデザイン", href: company_post_path(notification.post_id)) + "のオファー結果が届いています"
     end
   end
 
