9b5776d875c53b90071dbe089a23cd6e76a295d1
SUZUKIYUI <iaguminano@docomo.ne.jp>
2021-10-20 12:24:03 +0000

    [Fix]オファー通知記述修正

--- app/helpers/design_contributor/notification2s_helper.rb
+++ app/helpers/design_contributor/notification2s_helper.rb
@@ -1,7 +1,7 @@
 module DesignContributor::Notification2sHelper
 
   # 企業からきたオファーのみの通知を表示
-  def notification2_form(notification2)
+  def notification_design_contributor_offer_form(notification2)
     @visitor = notification2.company
 
     tag.a(@visitor.company_name, href: design_contributor_company_path(@visitor)) + "様から" + tag.a("あなたのデザイン", href: design_contributor_post_path(notification2.post_id)) + "にオファーが届きました"
