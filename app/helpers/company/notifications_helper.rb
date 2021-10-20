c9d4f2734b78073b357d46b6bc78f9ee0740efac
SUZUKIYUI <iaguminano@docomo.ne.jp>
2021-10-20 12:06:17 +0000

    [Fix]オファー通知機能記述修正

--- app/helpers/company/notifications_helper.rb
+++ app/helpers/company/notifications_helper.rb
@@ -1,11 +1,11 @@
 module Company::NotificationsHelper
 
   # デザイン投稿者からきたオファーのみの通知を表示
-  def notification_form(notification)
+  def notification_company_offer_form(notification)
     @visitor = notification.visitor
     case notification.action
       when "offer" then
-        tag.a("#{@visitor.pen_name}様のデザイン", href: company_post_path(notification.post)) + "のオファー結果が届いています"
+        tag.a("#{@visitor.pen_name}様のデザイン", href: company_post_path(notification.post)) + "の" + tag.a("オファー結果", href: company_company_path(notification.visitedable_id)) + "が届いています"
     end
   end
 
