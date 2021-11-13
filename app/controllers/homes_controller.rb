517118985f8da170e51088c8aedfb56f48edfd43
SUZUKIYUI <iaguminano@docomo.ne.jp>
2021-11-13 12:29:16 +0000

    [Fix]homesコントローラ/@want_posts(いいね順)定義修正

--- app/controllers/homes_controller.rb
+++ app/controllers/homes_controller.rb
@@ -1,10 +1,10 @@
 class HomesController < ApplicationController
 
   def top
+    # 新着のデザイン（新着順で12件まで表示）
     @new_posts = Post.all.order(created_at: :desc).limit(12)
-    target_post_ids = Post.joins(:offers).where('not offers.offer_status = 1').distinct.pluck(:id)
-    @want_posts = Post.joins(:wants).where(id: target_post_ids).group('wants.post_id').order('count(wants.post_id) desc').limit(3)
-
+    # 商品化希望の多いデザイン（オファー成立したものを除いて3つまで表示）
+    @want_posts = Post.joins(:wants).group('wants.post_id').order('count(wants.post_id) desc').where.not(id: (Post.joins(:offers).where('offers.offer_status = 1').distinct.pluck(:id))).limit(3)
   end
 
   def about
