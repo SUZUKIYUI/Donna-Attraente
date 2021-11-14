4a0aca7922ff77f29326dc1ae3401b5af156ad47
SUZUKIYUI <iaguminano@docomo.ne.jp>
2021-11-14 12:59:46 +0000

    [Fix]オファー通知記述修正

--- app/controllers/design_contributor/notification2s_controller.rb
+++ app/controllers/design_contributor/notification2s_controller.rb
@@ -4,7 +4,8 @@ class DesignContributor::Notification2sController < ApplicationController
 
   # 企業からデザイン投稿者にきたオファーの通知一覧
   def index
-    @notification2s = current_design_contributor.notification2s
+    # 現在ログイン中のユーザーの全ての「交渉中」のオファー通知
+    @notification2s = Notification2.where(id: (Notification2.joins(:offer).where('offers.offer_status = 0').pluck(:id)), design_contributor_id: current_design_contributor.id)
     @notification2s.where(checked: false).each do |notification2|
       notification2.update_attributes(checked: true)
     end
