e54a09c8291c9b91fda91e74442282bf97a04587
SUZUKIYUI <iaguminano@docomo.ne.jp>
2021-11-14 13:42:51 +0000

    [Fix]新着件数修正

--- app/controllers/homes_controller.rb
+++ app/controllers/homes_controller.rb
@@ -1,8 +1,8 @@
 class HomesController < ApplicationController
 
   def top
-    # 新着のデザイン（新着順で12件まで表示）
-    @new_posts = Post.all.order(created_at: :desc).limit(12)
+    # 新着のデザイン（新着順で6件まで表示）
+    @new_posts = Post.all.order(created_at: :desc).limit(6)
     # 商品化希望の多いデザイン（オファー成立したものを除いて3つまで表示）
     @want_posts = Post.joins(:wants).group('wants.post_id').order('count(wants.post_id) desc').where.not(id: (Post.joins(:offers).where('offers.offer_status = 1').distinct.pluck(:id))).limit(3)
   end
