9ef234391fec69e5bccde18a91435fdc1946584e
SUZUKIYUI <iaguminano@docomo.ne.jp>
2021-11-24 12:18:11 +0000

    [Update]default削除

--- db/migrate/20210627111006_create_posts.rb
+++ db/migrate/20210627111006_create_posts.rb
@@ -1,9 +1,9 @@
 class CreatePosts < ActiveRecord::Migration[5.2]
   def change
     create_table :posts do |t|
-      t.integer :design_contributor_id, null: false, default: ""
+      t.integer :design_contributor_id
       t.string :post_image_id, null: false, default: ""
-      t.text :author_comment, null: false, default: ""
+      t.text :author_comment
 
       t.timestamps
     end
